7ba76a53388cafc0f57cc5371cdc36fe3ebc1b70
yuihmoo <yuihmoo@gmail.com>
2022-03-22 15:23:07 +0900

    flutter-in-action 안드로이드 상위버전 호환 설정 완료

---
+++ lib/utils/weather_date_utils.dart
@@ -0,0 +1,11 @@
+class WeatherDateUtils {
+  static const Map<int, String> weekdays = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday",
+  };
+}
